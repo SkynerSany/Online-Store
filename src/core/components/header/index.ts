3344d3ff65eb09680ccb81444e2e6475d7aedb27
EfimenkoAndrey <andrey.efimenko.sp@gmail.com>
2022-12-25 20:28:34 +0300

    refactor: rewrite function and add checking elements

--- src/core/components/header/index.ts
+++ src/core/components/header/index.ts
@@ -4,29 +4,35 @@ import './header.scss';
 
 const headerElement: HTMLTemplateElement = stringToElement(HEADERTEMLATE);
 
-const addActiveClasessForMenu = ():void => {
-  const headerMenu = document.querySelector('.header__menu')as HTMLElement;
-  const burger = document.querySelector('.header__burger-img')as HTMLElement;
-  headerMenu.classList.toggle('header__menu_active');
-  burger.classList.toggle('menu-active');
+const toggleActiveClasessForMenu = ():void => {
+  const headerMenu = document.querySelector('.header__menu');
+  const burger = document.querySelector('.header__burger-img');
+  if (headerMenu instanceof HTMLElement && burger instanceof HTMLElement) {
+    headerMenu.classList.toggle('header__menu_active');
+    burger.classList.toggle('menu-active');
+  }
 }
 
 export const removeActiveClassesForMenu = (): void => {
-  const headerMenu = document.querySelector('.header__menu')as HTMLElement;
-  const burger = document.querySelector('.header__burger-img')as HTMLElement;
-  headerMenu.classList.remove('header__menu_active');
-  burger.classList.remove('menu-active');
+  const headerMenu = document.querySelector('.header__menu');
+  const burger = document.querySelector('.header__burger-img');
+  if (headerMenu instanceof HTMLElement && burger instanceof HTMLElement) {
+    headerMenu.classList.remove('header__menu_active');
+    burger.classList.remove('menu-active');
+  }
 }
 
 export const addListenerForMenu = ():void => {
-  const menuBtn = document.querySelector('.header__burger-menu')as HTMLElement;
-  menuBtn.addEventListener('click', () => {
-    addActiveClasessForMenu();
-  });
-  const arrMenuItems = document.querySelectorAll('.menu-item');
-  arrMenuItems.forEach((elem) => {
-    elem.addEventListener('click', removeActiveClassesForMenu);
-  })
+  const menuBtn = document.querySelector('.header__burger-menu');
+  if (menuBtn instanceof HTMLElement) {
+    menuBtn.addEventListener('click', () => {
+      toggleActiveClasessForMenu();
+    });
+    const arrMenuItems = document.querySelectorAll('.menu-item');
+    arrMenuItems.forEach((elem) => {
+      elem.addEventListener('click', removeActiveClassesForMenu);
+    });
+  }
 }
 
 
