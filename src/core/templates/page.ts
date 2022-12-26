4ba8474d73a67132f732480db15f59fb185e8325
EfimenkoAndrey <andrey.efimenko.sp@gmail.com>
2022-12-27 00:01:38 +0300

    init: start work above cart-page

--- src/core/templates/page.ts
+++ src/core/templates/page.ts
@@ -14,6 +14,22 @@ abstract class Page {
     return headerTitle;
   }
 
+  customCreateElement(tagName:string, className:string, text:string, value:string, type:string, min:string, max:string, step:string): HTMLElement {
+    const element = document.createElement(tagName);
+    if (className) element.className = className;
+    if (text) element.textContent = text;
+    if (value && element instanceof HTMLInputElement) element.value = value;
+    if (type && element instanceof HTMLInputElement || element instanceof HTMLButtonElement) element.type = type;
+    if (min && element instanceof HTMLInputElement) element.min = min;
+    if (max && element instanceof HTMLInputElement) element.max = max;
+    if (step && element instanceof HTMLInputElement) element.step = step;
+    return element;
+  }
+
+  appendElement(node: HTMLElement, element: HTMLElement | HTMLTemplateElement): void {
+    node.append(element);
+  }
+
   render() {
     return this.container;
   }
