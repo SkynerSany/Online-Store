74a662b4a49aaa0a07e9c0a13bfc01fe82ac5b5f
Alexandr Rylkov <skynerstudent@gmail.com>
2023-01-10 23:15:54 +0300

    fix: grammar mistake

--- src/core/components/modal-window/modal.template.ts
+++ src/core/components/modal-window/modal.template.ts
@@ -1,9 +1,9 @@
 const MODAL_TEMPLATE =  `<div class="modal-backDrop">
 <div class="modal">
-  <h2 class="modal__title">Офрмление заказа</h2>
+  <h2 class="modal__title">Оформление заказа</h2>
   <form action="" class="modal__form">
     <div class="modal__personal-details">
-      <input type="text" name="" id="person-name" placeholder="Имя" class="modal__input-name" required>
+      <input type="text" name="" id="person-name" placeholder="ФИО" class="modal__input-name" required>
       <input type="tel" name="" pattern="[+]+[0-9]{9,}" title="Формат +123456789" id="person-tel" placeholder="Телефон" class="modal__input-name" required>
       <input type="text" name="" id="person-address" placeholder="Адрес доставки" class="modal__input-name" required>
       <input type="email" name="" id="person-email" placeholder="Электронный адрес" class="modal__input-name" required>
